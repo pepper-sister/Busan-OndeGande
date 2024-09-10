e50be3ed4459915a8318a36e2ee7bda656f31430
yujeong223 <yuyu7123@naver.com>
2024-09-11 00:51:52 +0900

    하단 이메일 수정

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <footer className="footer">
       <div className="footer-content">
         <p>© 2024 Busan's OndeGande.  Reserved.</p>
-        <p>Contact: email@naver.com</p>
+        <p>Contact: ondegande010203@gmail.com</p>
       </div>
     </footer>
   );
