6d276f550404aa2ada64bb3f4a84b3114bda87a3
Ha-seunga <101798617+Ha-seunga@users.noreply.github.com>
2024-09-21 03:26:50 +0900

    Drag and Drop 완료, 일부디자인 및 공유 메시지 수정
    Day 1 초기화 부분 수정(드래그앤 드롭 기능 구현 완료, 
    일자 추가 경고, 
    사이드바 디자인 수정, 
    카카오톡 공유 일부 수정(장소명, 위도 경도 공유).

--- src/pages/MakingCourse.js
+++ src/pages/MakingCourse.js
@@ -1,16 +1,46 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef  } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd'; // drag and drop 때문에 추가
 import './MakingCourse.css';
 
 function MakingCourse() {
   const [map, setMap] = useState(null);
   const [places, setPlaces] = useState([]);
-  const [days, setDays] = useState([{ title: '당일치기', courses: [] }]);
+  const [days, setDays] = useState([]); //useState([{ title: 'Day 1', courses: [] }]); //DAY1 드래그 앤 드롭을 위해 제거
   const [selectedDayIndex, setSelectedDayIndex] = useState(0);
   const [sidebarLeftVisible, setSidebarLeftVisible] = useState(true); // 상태 추가
   const [sidebarRightVisible, setSidebarRightVisible] = useState(true); // 상태 추가
   const clipboardBtnRef = useRef(null);
 
+  // addDay를 useCallback으로 감싸서 의존성 문제 해결 했던것 ver 3
+  /*
+  const addDay = useCallback(() => {
+    const newDayIndex = days.length + 1;
+    const updatedDays = [...days, { title: `Day ${newDayIndex}`, courses: [] }];
+    setDays(updatedDays);
+    setSelectedDayIndex(updatedDays.length - 1);
+  }, [days]);*/
+
+  // addDay가 "일자 추가"버튼을 눌러야만 생성이 되고,MAx일정이 10개로 설정한 코드 ver 4
+  const addDay = () => {
+    if (days.length >= 10) {
+      alert("일자는 최대 10개까지 추가할 수 있습니다."); // 10개 이상 추가하려고 하면 경고
+      return;
+    }
+    const newDayIndex = days.length + 1;
+    const updatedDays = [...days, { title: `Day ${newDayIndex}`, courses: [] }];
+    setDays(updatedDays);
+    setSelectedDayIndex(updatedDays.length - 1); // 새로 추가된 Day를 선택된 Day로 설정
+  };
+
+//DAY 1 drag drop을 위해 다른 days처럼 동작하게끔 아래코드 추가
   useEffect(() => {
+    // 컴포넌트가 마운트될 때 Day 1을 동적으로 추가
+    // 이를 통해 Day 1을 다른 Day들과 동일한 방식으로 추가
+    //addDay(); 
+  }, []);
+
+  useEffect(() => {
+    // 카카오 맵을 로드하여 맵을 초기화하는 부분
     if (window.kakao && window.kakao.maps) {
       const container = document.getElementById('map');
       const options = {
@@ -23,6 +53,27 @@ function MakingCourse() {
       console.error('카카오 맵 API를 로드할 수 없습니다.');
     }
   }, []);
+
+  //새로운 addDay 함수 였던 것.
+  /*
+  const addDay = () => {
+    // 새로운 Day를 추가하는 함수
+    // days 배열의 길이에 따라 Day 1, Day 2 등의 타이틀을 자동으로 지정
+    const newDayIndex = days.length + 1;
+    const updatedDays = [...days, { title: `Day ${newDayIndex}`, courses: [] }];
+    setDays(updatedDays);
+    setSelectedDayIndex(updatedDays.length - 1); // 새로 추가된 Day를 선택된 Day로 설정
+  };
+*/
+    //기존 addDay함수
+    /*
+    const addDay = () => {
+    const newDayIndex = days.length + 1;
+    const updatedDays = [...days, { title: `Day ${newDayIndex}`, courses: [] }];
+    setDays(updatedDays);
+    setSelectedDayIndex(updatedDays.length - 1);
+  };
+  */
 
   useEffect(() => {
     const ClipboardJS = require('clipboard');
@@ -41,6 +92,15 @@ function MakingCourse() {
       clipboard.destroy();
     };
   }, []);
+
+  /*카카오 공유를 위해 추가한 코드*/
+ // index.js에서 한 번 초기화한 경우, 다른 컴포넌트에서는 아래처럼 확인만 하고 초기화하지 않음
+ useEffect(() => {
+  if (window.Kakao && !window.Kakao.isInitialized()) {
+    window.Kakao.init('32409dc8712c4b31d8017f4ad5dc076a');
+  }
+}, []);
+
 
   const searchPlaces = () => {
     if (!window.kakao || !window.kakao.maps) return;
@@ -77,11 +137,24 @@ function MakingCourse() {
   };
 
   const addCourse = (place) => {
+    //경고문 추가
+    if (days.length === 0) {
+      alert("일자를 먼저 추가하세요."); // 수정, 추가
+      return;
+    }
+
+
     const updatedDays = [...days];
     updatedDays[selectedDayIndex].courses.push({
       name: place.place_name,
       lat: place.y,
       lng: place.x,
+
+      //추가
+      region2: place.region_2depth_name,
+      region3: place.region_3depth_name,
+      region3h: place.region_3depth_h_name,
+
       order: updatedDays[selectedDayIndex].courses.length + 1
     });
     setDays(updatedDays);
@@ -95,34 +168,69 @@ function MakingCourse() {
     setDays(updatedDays);
   };
 
-  const moveUpCourse = (dayIndex, index) => {
-    if (index === 0) return;
-    const updatedDays = [...days];
-    const updatedCourses = [...updatedDays[dayIndex].courses];
-    [updatedCourses[index], updatedCourses[index - 1]] = [updatedCourses[index - 1], updatedCourses[index]];
-    updatedCourses.forEach((course, idx) => {
-      course.order = idx + 1;
-    });
-    updatedDays[dayIndex].courses = updatedCourses;
+  const deleteDay = (dayIndex) => {
+    const updatedDays = [...days].filter((_, index) => index !== dayIndex);
+    setDays(updatedDays);
+    if (selectedDayIndex >= updatedDays.length) {
+      setSelectedDayIndex(updatedDays.length - 1);
+    }
+    updateDayTitles(updatedDays);
+  };
+
+  
+
+  const updateDayTitles = (daysArray) => {
+    //Day의 타이틀을 Day 1 , Day 2등으로 순차적으로 업데이트하는 함수. 
+    const updatedDays = daysArray.map((day, index) => ({
+      ...day,
+      title: `Day ${index + 1}`
+    }));
     setDays(updatedDays);
   };
 
-  const moveDownCourse = (dayIndex, index) => {
-    if (index === days[dayIndex].courses.length - 1) return;
+  const onDragEnd = (result) => {
+    const { source, destination } = result;
+  
+    if (!destination) return; // 드래그가 목적지로 떨어지지 않으면 아무 작업도 하지 않음
+  
     const updatedDays = [...days];
-    const updatedCourses = [...updatedDays[dayIndex].courses];
-    [updatedCourses[index], updatedCourses[index + 1]] = [updatedCourses[index + 1], updatedCourses[index]];
-    updatedCourses.forEach((course, idx) => {
-      course.order = idx + 1;
-    });
-    updatedDays[dayIndex].courses = updatedCourses;
-    setDays(updatedDays);
-  };
-
-  const addDay = () => {
-    const newDayIndex = days.length + 1;
-    setDays([...days, { title: `${newDayIndex}일차`, courses: [] }]);
-    setSelectedDayIndex(days.length); // 새로 추가된 일자를 선택
+    const sourceDayIndex = Number(source.droppableId);
+    const destinationDayIndex = Number(destination.droppableId);
+  
+    if (sourceDayIndex === destinationDayIndex) {
+      // 같은 Day 내에서 코스 순서 변경하는 경우
+      const day = updatedDays[sourceDayIndex];
+      const [movedCourse] = day.courses.splice(source.index, 1);
+      day.courses.splice(destination.index, 0, movedCourse);
+      
+      // 코스의 순서를 1부터 다시 설정
+      day.courses = day.courses.map((course, index) => ({
+        ...course,
+        order: index + 1
+      }));
+  
+      setDays(updatedDays);
+    } else {
+      // 다른 Day로 코스를 이동하는 경우
+      const sourceDay = updatedDays[sourceDayIndex];
+      const destinationDay = updatedDays[destinationDayIndex];
+  
+      const [movedCourse] = sourceDay.courses.splice(source.index, 1);
+      destinationDay.courses.splice(destination.index, 0, movedCourse);
+      
+      // 코스 순서를 다시 설정 (각 Day에서)
+      sourceDay.courses = sourceDay.courses.map((course, index) => ({
+        ...course,
+        order: index + 1
+      }));
+      destinationDay.courses = destinationDay.courses.map((course, index) => ({
+        ...course,
+        order: index + 1
+      }));
+  
+      setDays(updatedDays);
+    }
+    updateDayTitles(updatedDays);   // 드래그 앤 드롭 후 Day 타이틀 업데이트
   };
 
   const generateShareText = () => {
@@ -131,15 +239,72 @@ function MakingCourse() {
         `${course.order}. ${course.name}\n위도: ${course.lat}\n경도: ${course.lng}\n`
       )).join('\n')}`
     ).join('\n\n');
+
+    /*
+    //카카오맵 api 분석후 다시 수정할예쩡
+    return days.map(day => 
+      `${day.title}\n${day.courses.map(course => (
+        `${course.order}. ${course.name} ${course.region2} ${course.region3} ${course.region3h}\n위도: ${course.lat}\n경도: ${course.lng}\n`
+      )).join('\n')}` 
+    ).join('\n\n');
+    */
   };
 
   const getShareLink = () => {
-    const shareText = generateShareText();
-    const encodedText = encodeURIComponent(shareText);
+    //const shareText = generateShareText();              // 이건 링크로 보낼 때 사용 (카카오톡)
+    //const encodedText = encodeURIComponent(shareText);  // 이건 링크로 보낼 때 사용 (카카오톡)
+  
+    // 카카오톡 공유 API 호출 예시
+    const kakaoLink = () => {
+      if (!window.Kakao) {
+        console.error('Kakao 객체를 찾을 수 없습니다.');
+        return;
+      }
+      /*
+      //링크로 공유하기 일때
+      window.Kakao.Share.sendDefault({
+        objectType: 'list',
+        headerTitle: '여행 코스',
+        headerLink: {
+          mobileWebUrl: 'https://www.ondegande.site',
+          webUrl: 'https://www.ondegande.site',
+        },
+        contents: days.map(day => ({
+          title: day.title,
+          description: day.courses.map(course => `${course.order}. ${course.name}`).join('\n'),
+          imageUrl: 'http://k.kakaocdn.net/dn/bDPMIb/btqgeoTRQvd/49BuF1gNo6UXkdbKecx600/kakaolink40_original.png',
+          link: {
+            mobileWebUrl: 'https://www.ondegande.site',
+            webUrl: 'https://www.ondegande.site',
+          },
+        })),
+        buttons: [
+          {
+            title: '웹으로 보기',
+            link: {
+              mobileWebUrl: 'https://www.ondegande.site',
+              webUrl: 'https://www.ondegande.site',
+            },
+          },
+        ],
+      });
+      */
+
+     
+      //텍스트로 공유하기
+      window.Kakao.Share.sendDefault({
+        objectType: 'text',
+        text: generateShareText(), // 텍스트 공유 설정
+        link: {
+          mobileWebUrl: 'https://www.ondegande.site',
+          webUrl: 'https://www.ondegande.site',
+        },
+      });
+      
+    };
+
     return {
-      googleDocs: `https://docs.google.com/document/create?usp=sharing&title=여행 코스&body=${encodedText}`,
-      twitter: `https://twitter.com/intent/tweet?text=${encodedText}`,
-      facebook: `https://www.facebook.com/sharer/sharer.php?u=${encodedText}`
+      kakaoTalkLink: kakaoLink
     };
   };
 
@@ -186,57 +351,48 @@ function MakingCourse() {
         </button>
         {sidebarRightVisible && (
           <>
-            <div className="day-selector">
-              <button onClick={addDay}>일자 추가하기</button>
-            </div>
-            {days.map((day, dayIndex) => (
-              <div key={dayIndex}>
-                <h2>{day.title}</h2>
-                <ul>
-                  {day.courses.map((course, index) => (
-                    <li key={index} className="course-item">
-                      <div className="course-header">
-                        <strong>{course.order}. {course.name}</strong>
-                        <button className="delete-button" onClick={() => deleteCourse(dayIndex, index)}>삭제</button>
-                      </div>
-                      <div className="course-coordinates">
-                        <p>위도: {course.lat}</p>
-                        <p>경도: {course.lng}</p>
-                        <div className="move-buttons">
-                          <button 
-                            className="move-up-button"
-                            onClick={() => moveUpCourse(dayIndex, index)}
-                            disabled={index === 0}
-                          >
-                            ▲
-                          </button>
-                          <button 
-                            className="move-down-button"
-                            onClick={() => moveDownCourse(dayIndex, index)}
-                            disabled={index === day.courses.length - 1}
-                          >
-                            ▼
-                          </button>
-                        </div>
-                      </div>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            ))}
-            <div className="share-links">
-              <button 
-                ref={clipboardBtnRef} 
-                data-clipboard-text={generateShareText()} 
-                className="share-link-button"
-              >
-                클립보드에 복사
-              </button>
-              <a href={getShareLink().twitter} target="_blank" rel="noopener noreferrer" className="share-link-button">
-                트위터로 공유
-              </a>
-    
-            </div>
+            <h2>코스 관리</h2>
+            <DragDropContext onDragEnd={onDragEnd}>
+              {days.map((day, dayIndex) => (
+                <Droppable key={dayIndex} droppableId={`${dayIndex}`}>
+                  {(provided) => (
+                    <div
+                      className="day-container"
+                      ref={provided.innerRef}
+                      {...provided.droppableProps}
+                    >
+                      <h3>{day.title}</h3>
+                      <button onClick={() => deleteDay(dayIndex)}>일자 삭제</button>
+                      {day.courses.map((course, courseIndex) => (
+                        <Draggable
+                        key={course.order} // `course.order`를 key로 사용
+                        draggableId={`${dayIndex}-${course.order}`} // `course.order`를 draggableId로 사용
+                        index={courseIndex}
+                      >
+                          {(provided) => (
+                            <div
+                              className="course-item"
+                              ref={provided.innerRef}
+                              {...provided.draggableProps}
+                              {...provided.dragHandleProps}
+                            >
+                              {course.order}. {course.name} {course.region2} {course.region3} {course.region3h}
+                              <button onClick={() => deleteCourse(dayIndex, courseIndex)}>삭제</button>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {provided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              ))}
+            </DragDropContext>
+            <button onClick={addDay}>일자 추가</button>
+            <button ref={clipboardBtnRef} data-clipboard-text={generateShareText()}>
+              클립보드에 복사
+            </button>
+            <button onClick={getShareLink().kakaoTalkLink}>카카오톡 공유</button>
           </>
         )}
       </div>
