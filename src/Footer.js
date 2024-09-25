1f3ba6ffbd01977164645008e113c6f95df85f88
yujeong223 <yuyu7123@naver.com>
2024-09-26 01:13:25 +0900

    출처 표기 수정

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,14 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <p>© 2024 Busan's OndeGande.  Reserved.</p>
+        <p>ⓒ 2024. Busan's OndeGande All rights reserved.</p>
         <p>Contact: ondegande010203@gmail.com</p>
+        <div className="icon-links">
+          <span>Icons by:</span>
+          <a href="https://kr.freepik.com/icon/chef-hat_1249659">smalllikeart</a>,
+          <a href="https://kr.freepik.com/icon/fuji-mountain_6094022">smalllikeart</a>,
+          <a href="https://kr.freepik.com/icon/hostel_1252011">smalllikeart</a>
+        </div>
       </div>
     </footer>
   );
