c459b4d44be979725e12b6e44e8431a5acc91fcc
yujeong223 <yuyu7123@naver.com>
2024-09-26 03:12:47 +0900

    리로드 추가

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,13 +9,20 @@ function Navbar() {
   const isRestaurantPage = location.pathname === '/restaurants';
   const isPlacePage = location.pathname === '/place';
   const isSleepPage = location.pathname === '/sleep';
+
+  const handleLinkClick = (path) => {
+    if (location.pathname === path) {
+      window.location.reload();
+    }
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         {isRestaurantPage || isPlacePage || isSleepPage ? (
           <img src={logo} alt="logo" />
         ) : (
-          <Link to="/">
+          <Link to="/" onClick={() => handleLinkClick('/')}>
             <img src={logo} alt="logo" />
           </Link>
         )}
@@ -23,16 +30,16 @@ function Navbar() {
       {!isRestaurantPage && !isPlacePage && !isSleepPage && (
       <ul className="navbar-menu">
         <li className={location.pathname === '/makingcourse' ? 'active' : ''}>
-          <Link to="/makingcourse">코스 짜보이소</Link>
+        <Link to="/makingcourse" onClick={() => handleLinkClick('/makingcourse')}>코스 짜보이소</Link>
         </li>
         <li className={location.pathname === '/doingnow' ? 'active' : ''}>
-          <Link to="/doingnow">인자 머하노?</Link>
+        <Link to="/doingnow" onClick={() => handleLinkClick('/doingnow')}>인자 머하노?</Link>
         </li>
         <li className={location.pathname === '/gothis' ? 'active' : ''}>
-          <Link to="/gothis">이래 가보이소</Link>
+        <Link to="/gothis" onClick={() => handleLinkClick('/gothis')}>이래 가보이소</Link>
         </li>
         <li className={location.pathname === '/youtuber' ? 'active' : ''}>
-          <Link to="/youtuber">유튜바 코스</Link>
+        <Link to="/youtuber" onClick={() => handleLinkClick('/youtuber')}>유튜바 코스</Link>
         </li>
       </ul>    
       )}
