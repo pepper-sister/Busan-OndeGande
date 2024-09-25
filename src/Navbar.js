cdcd3497f61e977a4438a9c4bf82abcdac2f1ba3
yujeong223 <yuyu7123@naver.com>
2024-09-26 03:47:19 +0900

    리로드 수정

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import logo from './logo.png';
 import './Navbar.css';
 
@@ -10,38 +10,32 @@ function Navbar() {
   const isPlacePage = location.pathname === '/place';
   const isSleepPage = location.pathname === '/sleep';
 
-  const handleLinkClick = (path) => {
-    if (location.pathname === path) {
-      window.location.reload();
-    }
-  };
-
   return (
     <nav className="navbar">
       <div className="navbar-logo">
         {isRestaurantPage || isPlacePage || isSleepPage ? (
           <img src={logo} alt="logo" />
         ) : (
-          <Link to="/" onClick={() => handleLinkClick('/')}>
+          <a href="/">
             <img src={logo} alt="logo" />
-          </Link>
+          </a>
         )}
       </div>
       {!isRestaurantPage && !isPlacePage && !isSleepPage && (
-      <ul className="navbar-menu">
-        <li className={location.pathname === '/makingcourse' ? 'active' : ''}>
-        <Link to="/makingcourse" onClick={() => handleLinkClick('/makingcourse')}>코스 짜보이소</Link>
-        </li>
-        <li className={location.pathname === '/doingnow' ? 'active' : ''}>
-        <Link to="/doingnow" onClick={() => handleLinkClick('/doingnow')}>인자 머하노?</Link>
-        </li>
-        <li className={location.pathname === '/gothis' ? 'active' : ''}>
-        <Link to="/gothis" onClick={() => handleLinkClick('/gothis')}>이래 가보이소</Link>
-        </li>
-        <li className={location.pathname === '/youtuber' ? 'active' : ''}>
-        <Link to="/youtuber" onClick={() => handleLinkClick('/youtuber')}>유튜바 코스</Link>
-        </li>
-      </ul>    
+        <ul className="navbar-menu">
+          <li className={location.pathname === '/makingcourse' ? 'active' : ''}>
+            <a href="/makingcourse">코스 짜보이소</a>
+          </li>
+          <li className={location.pathname === '/doingnow' ? 'active' : ''}>
+            <a href="/doingnow">인자 머하노?</a>
+          </li>
+          <li className={location.pathname === '/gothis' ? 'active' : ''}>
+            <a href="/gothis">이래 가보이소</a>
+          </li>
+          <li className={location.pathname === '/youtuber' ? 'active' : ''}>
+            <a href="/youtuber">유튜바 코스</a>
+          </li>
+        </ul>
       )}
     </nav>
   );
