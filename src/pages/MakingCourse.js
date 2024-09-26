3969047b0f7da96d787a21c8273061768ef5490f
yujeong223 <yuyu7123@naver.com>
2024-09-27 03:18:46 +0900

    지도 오류 수정

--- src/pages/MakingCourse.js
+++ src/pages/MakingCourse.js
@@ -29,13 +29,25 @@ function MakingCourse() {
       const container = document.getElementById('map');
       const options = {
         center: new window.kakao.maps.LatLng(35.1796, 129.0756),
-        level: 3,
+        level: 5,
       };
       const mapInstance = new window.kakao.maps.Map(container, options);
       setMap(mapInstance);
 
       const iw = new window.kakao.maps.InfoWindow({ zIndex: 1 });
       setInfoWindow(iw);
+
+      const handleResize = () => {
+        if (mapInstance) {
+          mapInstance.relayout();
+        }
+      };
+
+      window.addEventListener('resize', handleResize);
+
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
     } else {
       console.error('카카오 맵 API를 로드할 수 없습니다.');
     }
